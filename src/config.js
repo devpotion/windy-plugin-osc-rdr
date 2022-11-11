516920340e591c59511ab807bb22a7fb2087c95e
devpotion <117982486+devpotion@users.noreply.github.com>
2022-11-11 11:10:34 +0100

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    displayName: 'drag demo',
+    hook: 'contextmenu',
+    className: 'plugin-lhpane',
+    classNameMobile: 'plugin-mobile-bottom-slide',
+    exclusive: 'lhpane',
+    dependencies: [ 'https://unpkg.com/windyplugin-module-plugin-coordinator@x.x.x/dist/pluginCoordinator.js',
+                    'https://unpkg.com/windyplugin-module-infobox@x.x.x/dist/infobox.js']
+};
